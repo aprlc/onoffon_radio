5bd73ba17c92f17fe70bedd18b27d29e4bd428d1
aprlc26 <75934187+aprlc26@users.noreply.github.com>
2023-06-15 17:02:12 +0100

    Update website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,27 +7,24 @@
 <div id="container">
 	<div class="about">
 		<p>
-		SPACE Radio is an online radio station made by and for the students of UAL’s Creative Computing Institute (CCI). 
-		SPACE Radio aims to empower students to actively participate in creating, holding, exploring, and shaping space within and outside of CCI.
+			SPACE Radio is an online radio station made by and for the students of UAL’s Creative Computing Institute (CCI). SPACE Radio aims to empower students to actively participate in creating, holding, exploring, and shaping <i>space</i> within and outside of CCI.
 		</p>
 	</div>
 	<section id="radio">
+		<!-- svelte-ignore a11y-missing-attribute -->
 		<iframe id="player" frameborder="0" width="280" height="216" src="http://localhost:8080/embed/player?stream=auto&title=Now Playing"></iframe>
 	</section>
 	<section>
 		<h2>SCHEDULE</h2>
 		<hr />
-		<!-- Looping through the list of posts with svelte's {#each} syntax ref: https://svelte.dev/tutorial/each-blocks -->
 		<div class="posts">
 			{#each posts as post}
-				<a href={post.metadata.slug}>
-					<div class="post">
-						<h3>{post.metadata.title}</h3>
-						<span>{post.metadata.type}</span>
-						<span>{post.metadata.date}</span>
-						<span>{post.metadata.time}</span>
-					</div>
-				</a>
+				<div class="post-container">
+					<div class="grid-item"><span class="pp-neue">{post.metadata.date}</span></div>
+					<div class="grid-item"><span class="pp-neue">{post.metadata.time}</span></div>
+					<div class="grid-item"><span class="pp-neue">{post.metadata.title}</span></div>
+					<div class="grid-item">{post.metadata.desc}</div>
+				</div>
 			{/each}
 		</div>
 	</section>
@@ -36,9 +33,9 @@
 <!-- All the styling within .svelte files is scoped. Meaning any CSS in this file doesn't effect rest of the files -->
 <style>
 	section {
-		font-family: 'Almarai', sans-serif;
+		font-family: 'Toren', serif;
 		font-weight: 400;
-		font-size: 12px;
+		font-size: 14px;
 	}
 
 	hr {
@@ -59,23 +56,38 @@
 
 	.posts {
 		display: flex;
+		justify-content: space-between;
 		flex-direction: column;
 		gap: 0em;
 	}
 
-	.post {
-		display: flex;
-		justify-content: space-between;
-		gap: 2em;
-		align-items: center;
+	.pp-neue {
+		font-family: 'PP Neue Machina', sans-serif;
+	}
+
+	.post-container {
+		display: grid;
+  		grid-template-columns: 1fr 2fr;
+  		grid-gap: 10px;
+	}
+
+	.grid-item {
+  		padding: 0px;
 	}
 
 	#container {
 		display: flex;
 		flex-direction: column;
 		gap: 1em;
-		max-width: 50%;
+		width: 50%;
 		margin: auto;
 	}
+
+	@media(max-width: 780px){
+		#container {
+			width: 100%;
+		}
+	}
+
 
 </style>
