dba4fa56679b5697c343b1a1aea2d4d583383c5d
aprlc26 <75934187+aprlc26@users.noreply.github.com>
2023-11-30 16:41:56 +0000

    npm lock vercel

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -5,7 +5,7 @@
 /// <reference types="@sveltejs/kit" />
 
 /**
- * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
  * 
  * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
  * 
@@ -26,67 +26,126 @@
  * ```
  */
 declare module '$env/static/private' {
+	export const npm_package_exports___node_polyfills_types: string;
 	export const MANPATH: string;
+	export const npm_package_scripts_test_cross_platform_build: string;
+	export const npm_package_exports___vite_import: string;
+	export const npm_package_exports___hooks_import: string;
 	export const TERM_PROGRAM: string;
 	export const NODE: string;
+	export const npm_package_dependencies_sade: string;
 	export const INIT_CWD: string;
+	export const npm_package_devDependencies_typescript: string;
+	export const npm_package_devDependencies__types_marked: string;
+	export const npm_package_homepage: string;
+	export const npm_package_devDependencies_vite: string;
 	export const SHELL: string;
 	export const TERM: string;
-	export const npm_config_metrics_registry: string;
+	export const npm_package_dependencies_devalue: string;
 	export const TMPDIR: string;
 	export const HOMEBREW_REPOSITORY: string;
-	export const npm_config_global_prefix: string;
+	export const npm_package_scripts_lint: string;
 	export const CONDA_SHLVL: string;
+	export const npm_package_dependencies_set_cookie_parser: string;
 	export const TERM_PROGRAM_VERSION: string;
+	export const npm_package_dependencies_cookie: string;
 	export const MallocNanoZone: string;
 	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
-	export const COLOR: string;
-	export const npm_config_noproxy: string;
-	export const npm_config_local_prefix: string;
+	export const npm_config_registry: string;
+	export const npm_package_devDependencies_svelte_preprocess: string;
+	export const npm_package_repository_url: string;
+	export const npm_package_exports___node_import: string;
+	export const npm_package_description: string;
 	export const USER: string;
+	export const npm_package_exports___package_json: string;
+	export const npm_package_dependencies_esm_env: string;
+	export const npm_package_license: string;
 	export const COMMAND_MODE: string;
-	export const npm_config_globalconfig: string;
+	export const PNPM_SCRIPT_SRC_DIR: string;
+	export const npm_package_exports___import: string;
 	export const CONDA_EXE: string;
+	export const npm_package_repository_directory: string;
 	export const SSH_AUTH_SOCK: string;
+	export const npm_package_bin_svelte_kit: string;
 	export const __CF_USER_TEXT_ENCODING: string;
 	export const npm_execpath: string;
+	export const npm_package_devDependencies__types_sade: string;
+	export const npm_package_devDependencies_svelte: string;
+	export const npm_package_dependencies__sveltejs_vite_plugin_svelte: string;
+	export const npm_config_frozen_lockfile: string;
 	export const _CE_CONDA: string;
+	export const npm_package_scripts_postinstall: string;
 	export const PATH: string;
-	export const npm_package_json: string;
+	export const npm_package_devDependencies_rollup: string;
+	export const npm_package_dependencies_magic_string: string;
 	export const _: string;
-	export const npm_config_userconfig: string;
-	export const npm_config_init_module: string;
 	export const __CFBundleIdentifier: string;
 	export const npm_command: string;
+	export const npm_package_devDependencies__types_mime: string;
 	export const PWD: string;
+	export const npm_package_bugs_url: string;
 	export const npm_lifecycle_event: string;
-	export const EDITOR: string;
+	export const npm_package_types: string;
+	export const npm_package_devDependencies_marked: string;
+	export const npm_package_repository_type: string;
 	export const npm_package_name: string;
 	export const LANG: string;
+	export const npm_package_scripts_test_integration: string;
+	export const npm_package_devDependencies__types_connect: string;
+	export const npm_package_dependencies_mime: string;
+	export const npm_package_exports___node_polyfills_import: string;
+	export const npm_package_exports___types: string;
+	export const npm_package_scripts_test_cross_platform_dev: string;
+	export const npm_package_devDependencies_vitest: string;
 	export const XPC_FLAGS: string;
 	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+	export const npm_package_dependencies_tiny_glob: string;
+	export const npm_package_engines_node: string;
+	export const npm_package_dependencies_sirv: string;
 	export const npm_config_node_gyp: string;
 	export const npm_package_version: string;
 	export const XPC_SERVICE_NAME: string;
 	export const _CE_M: string;
+	export const npm_package_dependencies_undici: string;
+	export const npm_package_type: string;
 	export const HOME: string;
 	export const SHLVL: string;
+	export const npm_package_scripts_test: string;
+	export const npm_package_scripts_check_all: string;
 	export const VSCODE_GIT_ASKPASS_MAIN: string;
+	export const npm_package_exports___vite_types: string;
+	export const npm_package_exports___hooks_types: string;
 	export const HOMEBREW_PREFIX: string;
-	export const npm_config_cache: string;
+	export const npm_package_scripts_format: string;
+	export const npm_package_peerDependencies_vite: string;
 	export const LOGNAME: string;
 	export const CONDA_PYTHON_EXE: string;
 	export const npm_lifecycle_script: string;
+	export const npm_package_peerDependencies_svelte: string;
 	export const VSCODE_GIT_IPC_HANDLE: string;
+	export const npm_package_devDependencies__types_set_cookie_parser: string;
 	export const npm_config_user_agent: string;
+	export const npm_package_files_3: string;
+	export const npm_package_dependencies__types_cookie: string;
+	export const npm_package_files_2: string;
+	export const npm_package_devDependencies__types_node: string;
+	export const npm_package_devDependencies__playwright_test: string;
 	export const GIT_ASKPASS: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
 	export const HOMEBREW_CELLAR: string;
 	export const INFOPATH: string;
+	export const npm_package_files_1: string;
+	export const npm_package_devDependencies_dts_buddy: string;
+	export const npm_package_files_0: string;
+	export const npm_package_dependencies_kleur: string;
+	export const npm_package_exports___node_types: string;
+	export const npm_package_files_6: string;
+	export const npm_package_scripts_check: string;
+	export const npm_package_files_5: string;
 	export const npm_node_execpath: string;
-	export const npm_config_prefix: string;
+	export const npm_package_scripts_test_unit: string;
+	export const npm_package_files_4: string;
 	export const COLORTERM: string;
-	export const NODE_ENV: string;
 }
 
 /**
@@ -103,7 +162,7 @@ declare module '$env/static/public' {
 }
 
 /**
- * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/master/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env).
  * 
  * This module cannot be imported into client-side code.
  * 
@@ -116,69 +175,128 @@ declare module '$env/static/public' {
  */
 declare module '$env/dynamic/private' {
 	export const env: {
+		npm_package_exports___node_polyfills_types: string;
 		MANPATH: string;
+		npm_package_scripts_test_cross_platform_build: string;
+		npm_package_exports___vite_import: string;
+		npm_package_exports___hooks_import: string;
 		TERM_PROGRAM: string;
 		NODE: string;
+		npm_package_dependencies_sade: string;
 		INIT_CWD: string;
+		npm_package_devDependencies_typescript: string;
+		npm_package_devDependencies__types_marked: string;
+		npm_package_homepage: string;
+		npm_package_devDependencies_vite: string;
 		SHELL: string;
 		TERM: string;
-		npm_config_metrics_registry: string;
+		npm_package_dependencies_devalue: string;
 		TMPDIR: string;
 		HOMEBREW_REPOSITORY: string;
-		npm_config_global_prefix: string;
+		npm_package_scripts_lint: string;
 		CONDA_SHLVL: string;
+		npm_package_dependencies_set_cookie_parser: string;
 		TERM_PROGRAM_VERSION: string;
+		npm_package_dependencies_cookie: string;
 		MallocNanoZone: string;
 		ORIGINAL_XDG_CURRENT_DESKTOP: string;
-		COLOR: string;
-		npm_config_noproxy: string;
-		npm_config_local_prefix: string;
+		npm_config_registry: string;
+		npm_package_devDependencies_svelte_preprocess: string;
+		npm_package_repository_url: string;
+		npm_package_exports___node_import: string;
+		npm_package_description: string;
 		USER: string;
+		npm_package_exports___package_json: string;
+		npm_package_dependencies_esm_env: string;
+		npm_package_license: string;
 		COMMAND_MODE: string;
-		npm_config_globalconfig: string;
+		PNPM_SCRIPT_SRC_DIR: string;
+		npm_package_exports___import: string;
 		CONDA_EXE: string;
+		npm_package_repository_directory: string;
 		SSH_AUTH_SOCK: string;
+		npm_package_bin_svelte_kit: string;
 		__CF_USER_TEXT_ENCODING: string;
 		npm_execpath: string;
+		npm_package_devDependencies__types_sade: string;
+		npm_package_devDependencies_svelte: string;
+		npm_package_dependencies__sveltejs_vite_plugin_svelte: string;
+		npm_config_frozen_lockfile: string;
 		_CE_CONDA: string;
+		npm_package_scripts_postinstall: string;
 		PATH: string;
-		npm_package_json: string;
+		npm_package_devDependencies_rollup: string;
+		npm_package_dependencies_magic_string: string;
 		_: string;
-		npm_config_userconfig: string;
-		npm_config_init_module: string;
 		__CFBundleIdentifier: string;
 		npm_command: string;
+		npm_package_devDependencies__types_mime: string;
 		PWD: string;
+		npm_package_bugs_url: string;
 		npm_lifecycle_event: string;
-		EDITOR: string;
+		npm_package_types: string;
+		npm_package_devDependencies_marked: string;
+		npm_package_repository_type: string;
 		npm_package_name: string;
 		LANG: string;
+		npm_package_scripts_test_integration: string;
+		npm_package_devDependencies__types_connect: string;
+		npm_package_dependencies_mime: string;
+		npm_package_exports___node_polyfills_import: string;
+		npm_package_exports___types: string;
+		npm_package_scripts_test_cross_platform_dev: string;
+		npm_package_devDependencies_vitest: string;
 		XPC_FLAGS: string;
 		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+		npm_package_dependencies_tiny_glob: string;
+		npm_package_engines_node: string;
+		npm_package_dependencies_sirv: string;
 		npm_config_node_gyp: string;
 		npm_package_version: string;
 		XPC_SERVICE_NAME: string;
 		_CE_M: string;
+		npm_package_dependencies_undici: string;
+		npm_package_type: string;
 		HOME: string;
 		SHLVL: string;
+		npm_package_scripts_test: string;
+		npm_package_scripts_check_all: string;
 		VSCODE_GIT_ASKPASS_MAIN: string;
+		npm_package_exports___vite_types: string;
+		npm_package_exports___hooks_types: string;
 		HOMEBREW_PREFIX: string;
-		npm_config_cache: string;
+		npm_package_scripts_format: string;
+		npm_package_peerDependencies_vite: string;
 		LOGNAME: string;
 		CONDA_PYTHON_EXE: string;
 		npm_lifecycle_script: string;
+		npm_package_peerDependencies_svelte: string;
 		VSCODE_GIT_IPC_HANDLE: string;
+		npm_package_devDependencies__types_set_cookie_parser: string;
 		npm_config_user_agent: string;
+		npm_package_files_3: string;
+		npm_package_dependencies__types_cookie: string;
+		npm_package_files_2: string;
+		npm_package_devDependencies__types_node: string;
+		npm_package_devDependencies__playwright_test: string;
 		GIT_ASKPASS: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
 		HOMEBREW_CELLAR: string;
 		INFOPATH: string;
+		npm_package_files_1: string;
+		npm_package_devDependencies_dts_buddy: string;
+		npm_package_files_0: string;
+		npm_package_dependencies_kleur: string;
+		npm_package_exports___node_types: string;
+		npm_package_files_6: string;
+		npm_package_scripts_check: string;
+		npm_package_files_5: string;
 		npm_node_execpath: string;
-		npm_config_prefix: string;
+		npm_package_scripts_test_unit: string;
+		npm_package_files_4: string;
 		COLORTERM: string;
-		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
-		[key: `${string}`]: string | undefined;
+		[key: string]: string | undefined;
 	}
 }
 
