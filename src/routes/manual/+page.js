32daea8bdc282a28a76f99af178960443c97f0ec
aprlc26 <75934187+aprlc26@users.noreply.github.com>
2023-12-06 23:50:28 +0000

    move readme

--- src/routes/manual/+page.js
+++ src/routes/manual/+page.js
@@ -1,7 +1,8 @@
 import { error } from '@sveltejs/kit';
 
 export function load() {
-	const postsModules = import.meta.glob('./manual.md', { eager: true });
+	// const postsModules = import.meta.glob('./manual.md', { eager: true });
+	const postsModules = import.meta.glob('../../README.md', { eager: true });
     
 	let posts = Object.values(postsModules)[0];
     
